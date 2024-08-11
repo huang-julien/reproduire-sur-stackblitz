ad28b95c9737a7033e6c899f370d4389b5906be2
Julien Huang <julien.h.dev@gmail.com>
2024-08-11 16:59:55 +0200

    feat: add first implementation

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import { context, getOctokit } from "@actions/github"
+import { setFailed, info, getInput } from "@actions/core"
+import { getStackblitzLinkMessage } from "./message"
+
+async function main() {
+    try {
+        const {
+            repo,
+            payload: {
+                issue,
+                pull_request: pullRequest,
+            }
+        } = context
+        if (pullRequest || !issue?.body) return info("Not an issue or has no body.")
+
+        const result = issue.body.match(/(?:^|\n)#+\s[^\n]*\n(.*?)(?=\n##?\s|$)/gm)?.map(v => v.trim())
+
+        if (!result) return info("No heading found")
+
+        const reproductionSection = result.find(v => v.toLowerCase().startsWith(getInput('reproduction-section')))
+        if (!reproductionSection) return info("No reproduction section found")
+
+        const ghLink = reproductionSection.match(/github\.com\/([^/ ]+\/[^/ ]+)/g)
+        if (!ghLink || !ghLink.length) return info("No github repo found")
+
+        const linkRepo = ghLink[0].replace(/github\.com\/([^/ ]+\/[^/ ]+)/g, (match, repo) => {
+            info(`Found repo: ${repo}`)
+            return repo
+        })
+
+        const token = getInput("repo-token") ?? process.env.GITHUB_TOKEN
+        const client = getOctokit(token).rest
+        client.issues.createComment({
+            ...repo,
+            issue_number: issue.number,
+            body: getStackblitzLinkMessage(linkRepo),
+        })
+
+        return info(`Comment created for issue: ${issue.number} to the repo: ${linkRepo} on stackblitz`)
+    }
+    catch (error) {
+        setFailed(error instanceof Error ? error.message : "Unknown error")
+    }
+}
+
+
+main()
---
+++ src/message.ts
@@ -0,0 +1,5 @@
+export const getStackblitzLinkMessage = (repo: string) => {
+    return `
+Stackblitz link for the reproduction: [Stackblitz](https://stackblitz.com/github/${repo})
+`
+}
